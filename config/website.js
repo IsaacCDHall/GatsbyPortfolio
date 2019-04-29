88f78322d759a284416bda99ce9aeb806873c210
Isaac Hall <isaacdehall@gmail.com>
2019-04-28 18:43:28 -0700

    inital staging edits

--- config/website.js
+++ config/website.js
@@ -3,23 +3,27 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Isaac Hall Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'IsaacHall', // Alternative Site title for SEO
+  siteTitleShort: 'Isaac', // short_name for manifest
+  siteHeadline: 'Aspiring Developer Portfolio', // Headline for schema.org JSONLD
+  siteUrl: 'https://isaachall.dev', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Parallax animatated single page portfolio',
+  author: 'Isaac Hall', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@NA', // Twitter Username
+  ogSiteName: 'isaachall', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: 'UA-139172809-1',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
   backgroundColor: tailwind.colors.blue,
 }
+ // delete Twitter & facebook
+
+
+//robots.txt
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -36,6 +36,9 @@ const ProjectsWrapper = styled.div`
 const AboutHero = styled.div`
   ${tw`flex flex-col lg:flex-row items-center mt-8`};
 `
+const Pokemon = styled.div`
+  font-size: 60%;
+`
 
 const Avatar = styled.img`
   ${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto`};
@@ -63,73 +66,70 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Hey, <br /> I'm Isaac Hall.
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>I am an aspiring developer currently getting my hands dirty with Javascript frameworks.</Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
+            title="SpongeBob Taproom"
+            link="https://github.com/IsaacCDHall/Kegger-React"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+            My most recent work practicing state management in React.js.
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
+            title="Du son Jardin"
+            link="https://github.com/IsaacCDHall/du-son-Jardin"
             bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+            I lead a team to create a music visualizer/ MIDI board.
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+            title="Goyard"
+            link="https://github.com/IsaacCDHall/GoHardGoyard"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
+            Replica of Goyard's Home Page using just CSS and HTMl
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
+            title="DocTalk"
+            link="https://github.com/IsaacCDHall/DocTalk"
             bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+            Find local doctors - utilizes BetterDoctor api call.
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
       <About offset={3}>
         <Title>About</Title>
         <AboutHero>
-          <Avatar src={avatar} alt="John Doe" />
+          <Avatar src={avatar} alt="Isaac Hall" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+            <em>"I want to be the very best <br/> Like no one ever was. <br/> To catch bugs is my real test <br/> To debug is my cause!"</em>
+            <br/><br/>
+            <Pokemon>Pokémon&copy;</Pokemon>
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+          I am an aspiring developer currently getting my hands dirty with Javascript frameworks. My experience in marketing, branding, and developing teams inspires me to create beautiful user experiences and efficient apps. I am excited to elevate my skills to the next level through code.
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            <a href="mailto:isaachalldev@gmail.com">Email</a> or find me on other platforms:{' '}
+            <a href="https://github.com/IsaacCDHall">Github</a> &{' '}
+            <a href="https://www.linkedin.com/in/isaacdev/">LinkedIn</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          &copy; 2019 Isaac Hall.{' '}
+          <a href="https://github.com/IsaacCDHall/GatsbyPortfolio">Github Repository</a>. Inspired by{' '}
+          <a href="https://www.awwwards.com/websites/parallax/">awwards&trade; </a>Parallax designs.
         </Footer>
       </Contact>
     </Parallax>
